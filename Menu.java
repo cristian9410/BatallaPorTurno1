87be08266087fd4b9aa6d4a62ff98d007dbaafdd
Cristian Alcantara <38290019+cristian9410@users.noreply.github.com>
2018-05-27 19:47:15 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,29 +1,25 @@
 package com.cristianAlcantara.batallaPorTurno;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-
-/////////////////////////////////////////////////////////////////////////
-//////////////////ESTA CLASE MENU ES PARA PROBAR/////////////////////////
-////////////////////////////////////////////////////////////////////////
 public class Menu {
-
+	 
+	public static String nombreP2;	
+	public static String nombreP1;    
+        
 	public static void menu () {
 		
 		int exit = 0;
-		do {
-			
-			System.out.println("BIENVENIDO A BATALLA DE MASCOTAS \n");
-			
+		do {			
+			System.out.println("BIENVENIDO A BATALLA DE MASCOTAS \n");			
 			System.out.println("Precione la opcion deseada");
 			System.out.println("1. Selecion de personajes");	
 			System.out.println("2. Empezar Batalla");
-			System.out.println("0. Salir");
-			
+			System.out.println("0. Salir");			
 			
 			Scanner sc1 = new Scanner(System.in);
-			int response = Integer.valueOf(sc1.nextLine());
-		
+			int response = Integer.valueOf(sc1.nextLine());		
 
 			switch (response) {
 				case 0:	//salir	
@@ -34,24 +30,20 @@ public class Menu {
 					break;				
 				case 2:Batalla.batalla();	//Empieza la pelea
 				default:						
-					System.out.println("\n \n ....¡¡Selecciona una opción valida 1!!....\n \n");		
+					System.out.println("\n \n ....Â¡Â¡Selecciona una opciÃ³n valida 1!!....\n \n");		
 					menu();
 					break;
 			}				
-		}while(exit != 0);			
-		
+		}while(exit != 0);		
 	}
 
 	//Seleccion de personaje
-	public static void seleccionDePersonaje() {	
-		
+	public static void seleccionDePersonaje() {			
 		int exit = 0;
-		do {
-			
+		do {			
 			System.out.println("1. Player 1 \n");				
 			System.out.println("2. Player 2 \n");
-			System.out.println("3. Atras \n");
-						
+			System.out.println("3. Atras \n");						
 			System.out.println("0. Salir");
 			
 			Scanner sc2 = new Scanner(System.in);
@@ -68,58 +60,25 @@ public class Menu {
 				case 3:
 					menu();	//Regresa atras
 				default:						
-					System.out.println("\n \n ....¡¡Selecciona una opción valida 2!!....\n \n");						
+					System.out.println("\n \n ....Â¡Â¡Selecciona una opciÃ³n valida 2!!....\n \n");						
 					break;
 			}				
 		}while(exit != 0);			
 	}
-
 	public static void seleccionPl1 () {	//Menu del player 1
 		
-		
-		int exit = 0;
-		do {
-			
-			System.out.println("PLAYER 1 \n");
-			
-			
-			System.out.println("Oprime el numero del personaje deseado");
-			System.out.println("1. Bicho");		
-			System.out.println("2. Boris");
-			System.out.println("3. Luna");
-			System.out.println("0. Atras");						
-			
-			Scanner sc3 = new Scanner(System.in);
-			int response = Integer.valueOf(sc3.nextLine());	
-			
-			switch (response) {
-				case 0:	//salir	
-					seleccionDePersonaje();
-					break;					
-				case 1:	
-					Personaje.player1.nombre = "Bicho";
-					Personaje.player1.vida = 150;						
-					seleccionDePersonaje();
-					break;	
-				case 2:
-					Personaje.player1.nombre = "Boris";
-					Personaje.player1.vida = 150;						
-					seleccionDePersonaje();
-					break;		
-				case 3:
-					Personaje.player1.nombre = "Luna";
-					Personaje.player1.vida = 150;						
-					seleccionDePersonaje();
-					break;
-				default:						
-					System.out.println("\n \n ....¡¡Selecciona una opción valida 3!!....\n \n");						
-					break;
-			}		
-			
-		}while(exit != 0);			
+		nombreP1 = JOptionPane.showInputDialog("NOMBRE DEL PLAYER 1");			
+		Personaje.player1.setNombre (nombreP1);		
+		Personaje.player1.setVida(150);						
+		seleccionDePersonaje();		
 	}
 
-
 	public static void seleccionPl2 () {	//Menu del player 2
+		
+		nombreP2 = JOptionPane.showInputDialog("NOMBRE DEL PLAYER 2");	
+		Personaje.player2.setNombre(nombreP2);	
+		Personaje.player2.setVida(150);
+		seleccionDePersonaje();	
 }
 }
+
